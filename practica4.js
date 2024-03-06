ebce5f15cc7b81e2ae256aaf3674a8aea00828c5
teresacab17 <teresa.caast@gmail.com>
2024-03-06 21:20:45 +0100

    estructura basica

---
+++ practica4.js
@@ -0,0 +1,41 @@
+/*exports.nombreFuncion = (parametros) => {
+   
+    return valor;
+}*/
+
+exports.load = (citiesFilename) => {
+   
+}
+
+exports.max_temp = (cities) => {
+
+}
+
+exports.min_temp = (cities) =>{
+
+}
+
+exports.average_temp = (cities) => {
+
+}
+
+exports.warmer_average_temp =(cities) => {
+
+}
+
+exports.max_north = (cities) => {
+
+}
+
+exports.max_south = (cities) => {
+
+}
+
+exports.gravity_center = (cities) => {
+
+}
+
+exports.closest_GC = (cities) => {
+
+}
+
